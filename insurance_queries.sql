f18344d2deeec1fb000ceee5505bc40d54f8e36b
MaryamGoudarzi-data <mgoudarzi242@gmail.com>
2025-05-06 12:22:12 +1200

    Add notebook, dataset, and EDA plots

---
+++ insurance_queries.sql
@@ -0,0 +1,11 @@
+CREATE TABLE insurance_data (
+age INT,
+sex VARCHAR(10),
+bmi FLOAT,
+children INT,
+smoker VARCHAR(10),
+region VARCHAR(50),
+charges FLOAT
+);
+
+SELECT * FROM insurance_data LIMIT 10;
